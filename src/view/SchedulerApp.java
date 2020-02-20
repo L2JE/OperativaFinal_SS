eea9b09ef2503d344569ab5ce0ffe23904d627df
conra <cblanco@alumnos.exa.unicen.edu.ar>
2020-02-20 01:51:20 -0300

    Codigo inicial con app javafx

---
+++ src/view/SchedulerApp.java
@@ -0,0 +1,26 @@
+package view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SchedulerApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // just load fxml file and display it in the stage:
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("scheduler.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
