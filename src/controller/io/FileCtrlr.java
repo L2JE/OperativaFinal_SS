1bef069fe800b72ffa58939f47cdbf905ad7f21c
L2JE <leojesus.juarez@gmail.com>
2020-03-12 16:25:25 +0100

    Definidos en FileCtrlr metodo importCSV y en los demas metodos esta solo la estructura basica.
    Metodo test en Main para importCSV.

--- src/controller/io/FileCtrlr.java
+++ src/controller/io/FileCtrlr.java
@@ -1,17 +1,23 @@
 package controller.io;
 
-import java.io.File;
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class FileCtrlr {
-    public static short importCSV(String path){
+    public static short importCSV(String path, ArrayList<String> data){ //cada String contiene elementos separados por comas
         try{
             FileReader iFile = new FileReader(path);
+            BufferedReader bLine = new BufferedReader(iFile);
+            String line;
 
+            while ((line = bLine.readLine()) != null){
+                data.add(line);
+            }
 
+            iFile.close();
             return 1;
         }catch (IOException e){
             return -1;
@@ -20,22 +26,26 @@ public class FileCtrlr {
         
     }
     
-    public static short exportCSV(String path, ArrayList<Object> contenedores){
+    public static short exportCSV(String path, ArrayList<String> content){
         try{
             FileWriter oFile = new FileWriter(path);
 
+            //PROCESO DE ESCRITURA EN CSV
 
+            oFile.close();
             return 1;
         }catch (IOException e){
             return -1;
         }
     }
     
-    public static short exportXLS(String path, ArrayList<Object> contenedores){
+    public static short exportXLS(String path, ArrayList<String> content){
         try{
             FileWriter oFile = new FileWriter(path);
 
+            //PROCESO DE ESCRITURA EN EXCEL EMBELLECIDO
 
+            oFile.close();
             return 1;
         }catch (IOException e){
             return -1;
--- src/view/Main.java
+++ src/view/Main.java
@@ -6,6 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import controller.io.FileCtrlr;
+
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -23,5 +28,17 @@ public class Main extends Application {
         primaryStage.setTitle("SchedulerSoft FACULTAD DE CS. HUMANAS");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        //runTestImportCSV();
+
+    }
+
+    public void runTestImportCSV(){
+        ArrayList<String> elementos = new ArrayList<String>();
+        FileCtrlr.importCSV("D:\\Users\\leo_c\\Desktop\\TP_Opertativa\\Datos\\materias.txt", elementos);
+
+        for(String s : elementos){
+            System.out.println(s);
+        }
     }
 }
