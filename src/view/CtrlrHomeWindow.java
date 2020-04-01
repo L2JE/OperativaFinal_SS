64e205c7b8d82c1b9782d1f37947b9f44e05f48e
L2JE <leojesus.juarez@gmail.com>
2020-04-01 17:32:09 -0300

    Agregados e implementados ItemView, itemViewFactory en view.customView para customCell. Test Test/Debug

--- src/view/CtrlrHomeWindow.java
+++ src/view/CtrlrHomeWindow.java
@@ -16,23 +16,7 @@ public class CtrlrHomeWindow {
     public ListView viewClassrooms;
 
     public CtrlrHomeWindow(){
-        if(true){//Error al cargar
-            /*
-            viewSubjects = new ListView();
-            viewRanges = new ListView();
-            viewCareers = new ListView();
-            viewClassrooms = new ListView();*/
-        }else{//Cargar de archivos .CSV
 
-        }
-        /*
-        viewSubjects.setCellFactory(new ItemViewFactory());
-        viewRanges.setCellFactory(new ItemViewFactory());
-        viewCareers.setCellFactory(new ItemViewFactory());
-        viewClassrooms.setCellFactory(new ItemViewFactory());
-        */
-        ObservableList<String> seasonList = FXCollections.<String>observableArrayList("Spring", "Summer", "Fall", "Winter");
-        viewSubjects.setItems(seasonList);
     }
 
 }
---
+++ src/view/customView/ItemView.java
@@ -0,0 +1,59 @@
+package view.customView;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+
+/**
+ * PARA GENERALIZAR EXTENDER DE LISTCELL<Object> o LISTCELL<MiClase>
+ * SOLO FUNCIONA CON LISTVIEW<String>
+ *
+ */
+public class ItemView extends ListCell<String> {
+    HBox hBox = new HBox();
+    Label label = new Label("vacio");
+    Pane p = new Pane();
+    Button infoB = new Button("(i)");
+    Button delB = new Button("(-)");
+
+    public ItemView(){
+        hBox.setSpacing(5.0);
+        hBox.getChildren().addAll(label, p, infoB,  delB);
+        HBox.setHgrow(p, Priority.ALWAYS);
+
+        delB.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(getIndex() + " : " + event);
+
+                ListView<String> cont = getListView();
+                cont.getItems().remove(getIndex());
+                cont.refresh();
+            }
+        });
+    }
+
+    @Override
+    public void updateItem(String item, boolean empty)
+    {
+        super.updateItem(item, empty);
+
+        int index = this.getIndex();
+
+        // Format name
+        if (item == null || empty)
+        {
+        }
+        else
+        {
+            label.setText(item);
+        }
+        setGraphic(hBox);
+    }
+}
