ebf8515860db5379396cd4bb76d01df7170b9bef
L2JE <leojesus.juarez@gmail.com>
2021-01-12 13:46:25 -0300

    update las commit

--- src/controller/HomeWindowCntlr.java
+++ src/controller/HomeWindowCntlr.java
@@ -1,15 +1,9 @@
 package controller;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import data_access.CareerDTO;
 import javafx.event.ActionEvent;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.input.InputMethodEvent;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.*;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 import model.Course;
@@ -22,13 +16,59 @@ public class HomeWindowCntlr {
     public ListView viewRanges;
     public ListView viewCareers;
     public ListView viewClassrooms;
-    public ComboBox<String> comboProfesor;
+    public ComboBox yearsCBCareer;
+    public ComboBox hInicCarrera;
+    public ComboBox hFinCarrera;
+    public TextField nameFieldCareer;
 
     public void initialize(){
+        initCarrera();
         viewSubjects.setCellFactory(new ItemViewFactory());
+        viewCareers.setCellFactory(new ItemViewFactory());
         runTestCustomItems();
     }
 
+    private void initCarrera(){
+        yearsCBCareer.setVisibleRowCount(3);
+        hInicCarrera.setVisibleRowCount(3);
+        hFinCarrera.setVisibleRowCount(3);
+        fillChoiceBox(yearsCBCareer, 1, 8, 'i');
+        fillChoiceBox(hInicCarrera, 8, 21, 't');
+        fillChoiceBox(hFinCarrera, 8, 22, 't');
+    }
+
+    private void fillChoiceBox(ComboBox cb, int min, int max, char format){
+        String[] values = new String[max-min+1];
+        String separator;
+
+        switch (format){
+            case 't':   separator = ":00hs";
+                break;
+            default:    separator = "";
+                break;
+        }
+
+        for(int i = 0; min <= max; i++, min++)
+            values[i] = min + separator;
+
+        cb.getItems().addAll(values);
+    }
+
+    public void addCarreraPressed(ActionEvent actionEvent) {
+        int id = 0;
+        int duration = -1;
+
+        /*String careerName = nameFieldCareer.getText();
+        int chosenStartTime = (int)hInicCarrera.getValue();
+        int chosenEndTime = (int)hFinCarrera.getValue();
+*/
+        ///validate duration
+        if(yearsCBCareer.getSelectionModel().getSelectedItem() != null)
+            System.out.println((int)yearsCBCareer.getSelectionModel().getSelectedItem());
+
+  //      CareerDTO newCareer = new CareerDTO(id, careerName, duration, chosenStartTime, chosenEndTime);
+        System.out.println("Carrera Agregada!");
+    }
 /** TEST:
  *  CLICK EN LISTA "MATERIAS" AGREGAR 5 ELEMENTOS
  **/
--- src/data_access/CareerDTO.java
+++ src/data_access/CareerDTO.java
@@ -1,4 +1,61 @@
 package data_access;
 
-public class CareerDTO {
+import model.Showable;
+
+public class CareerDTO implements Showable {
+    private int idCareer;
+    private String name;
+    private int years;
+    private int preferredStart;
+    private int preferredEnd;
+
+    public CareerDTO(int idCareer, String name, int years, int preferredStart, int preferredEnd) {
+        this.idCareer = idCareer;
+        this.name = name;
+        this.years = years;
+        this.preferredStart = preferredStart;
+        this.preferredEnd = preferredEnd;
+    }
+
+    public int getIdCareer() {
+        return idCareer;
+    }
+
+    public void setIdCareer(int idCareer) {
+        this.idCareer = idCareer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public int getPreferredStart() {
+        return preferredStart;
+    }
+
+    public int getPreferredEnd() {
+        return preferredEnd;
+    }
+
+    public void setPreferredBand(int preferredStart, int preferredEnd){
+        this.preferredStart = preferredStart;
+        this.preferredEnd = preferredEnd;
+    }
+
+    @Override
+    public String toString() {
+        return name +" ("+ years + "años)";
+    }
 }
